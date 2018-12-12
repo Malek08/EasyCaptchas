9a238124115c36567f06c7ff7ba7acc25f4fd7d4
Malek08 <45629268+Malek08@users.noreply.github.com>
2018-12-12 14:33:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,3 +9,19 @@ $_SESSION[‘code’]
 echo "<img src='script-captchas.php' alt='captchas' />";
 
 ?>
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	</head>
+			<body>
+				Veuillez saisir le code de sécurité !
+				<br>
+				<img src='script-captchas.php' alt='captchas'>
+				<br>
+				<form method="post" action="">
+					<input type="text" name='xcode'>
+					<input type="submit" name ="vcode" value ="VALIDER">
+				</form>
+			</body>
+</html>

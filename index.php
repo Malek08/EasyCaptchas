5a82b393ef826f7d05177baa3d9c61246b613364
Malek08 <45629268+Malek08@users.noreply.github.com>
2018-12-12 14:32:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 
 /* SCRIPT FRONTAL AFFICHAGE DE CAPTCHA
 * AFFICHER UNE IMAGE GÉNÉRÉE EN PHP */
-
+SESSION_START ()
+$_SESSION[‘code’]
+  
 echo "<img src='script-captchas.php' alt='captchas' />";
 
 ?>

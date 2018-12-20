e8ada9fb1c299a70aa715dd788dfd986f2c1fa41
Malek08 <45629268+Malek08@users.noreply.github.com>
2018-12-20 14:09:50 +0100

    Add color text
    Now the unvalidated message and the empty messages are red. The validated message is green.

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 session_start();
 				if(isset($_POST['captcha'])){
 				if($_POST['captcha']==$_SESSION['code']){
-				echo "Code correct";
+				echo '<p><span class="green">Code sécurité valide</span></p>';
 				}
 				if($_POST['captcha']!=$_SESSION['code']&&(!empty($_POST['captcha']))){
-				echo "Code incorrect";
+				echo '<p><span class="red">Code sécurité invalide</span></p>';
 				}
 				if (empty($_POST['captcha'])) {
 				{
-				echo "Champ vide";
+				echo '<p><span class="red">Code sécurité doit être impérativement saisi</span></p>';
 				}
 				}
 				header( 'content-type: text/html; charset=utf-8' );
